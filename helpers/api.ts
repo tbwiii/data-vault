bf3e2fb7986caab6445d651477b59f6f8e9c16a8
Ted Waller <ted.waller.iii@gmail.com>
2024-08-20 16:53:37 -0400

    nuxt env

--- helpers/api.ts
+++ helpers/api.ts
@@ -1,7 +1,7 @@
 export default {
     $post: async (dest:string, config:{}) => {
         return await (
-            await fetch(`${process.env.NEXT_PUBLIC_API_URL}${dest}`,{
+            await fetch(`${process.env.NEXT_PUBLIC_VERCEL_URL}/api${dest}`,{
                 method: "POST",
                 cache: 'no-store',
                 body: JSON.stringify(config),
