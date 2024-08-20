066292dced99f5c23b6037f5be58e28370c205b0
Ted Waller <ted.waller.iii@gmail.com>
2024-08-20 16:15:22 -0400

    update usage

--- helpers/api.ts
+++ helpers/api.ts
@@ -1,7 +1,7 @@
 export default {
     $post: async (dest:string, config:{}) => {
         return await (
-            await fetch(`${process.env.API_URL}${dest}`,{
+            await fetch(`${process.env.NEXT_PUBLIC_API_URL}${dest}`,{
                 method: "POST",
                 cache: 'no-store',
                 body: JSON.stringify(config),
