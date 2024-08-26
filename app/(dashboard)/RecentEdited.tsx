6e6ae5b3f570d8176aa94475bb9568917c905bf5
Ted Waller <ted.waller.iii@gmail.com>
2024-08-26 01:23:03 -0400

    add skeletons

--- app/(dashboard)/RecentEdited.tsx
+++ app/(dashboard)/RecentEdited.tsx
@@ -1,6 +1,7 @@
 import { Suspense } from 'react';
 import { getEntreies } from '@schema/entries';
 import EntryList from './EntryList';
+import {Skeleton} from '@/components/ui/skeleton';
 
 export default async function RecentCreated() {
     const { entries } = await getEntreies({ orderBy: 'updatedAt', limit: 10, direction: 'desc', where: { private: false } });
@@ -8,7 +9,15 @@ export default async function RecentCreated() {
     return (
         <div>
             <h2 className='font-bold mb-4'>Recently Edited</h2>
-            <Suspense fallback={"loading..."}>
+            <Suspense fallback={
+                <div className="opacity-60">
+                    <div className=''>
+                        {Array.from({ length: 8 }).map((_, index) => (
+                            <Skeleton key={index} className='mb-3 h-16 rounded' />
+                        ))}
+                    </div>
+                </div>
+            }>
                 <EntryList entries={entries} updatedAt={true}></EntryList>
             </Suspense>          
         </div>
--- app/[slug]/page.tsx
+++ app/[slug]/page.tsx
@@ -3,6 +3,7 @@ import Entry from './Entry';
 import { auth } from '@lib/auth';
 import { getEntryBySlug } from '@schema/entries';
 import { Suspense } from 'react';
+import { Skeleton } from "@/components/ui/skeleton"
 import './markdown.css';
 
 export default async function EntryPage({ params }: { params: { slug: string } }) {
@@ -13,7 +14,16 @@ export default async function EntryPage({ params }: { params: { slug: string } }
     const inferredTitle = params.slug.replace(/%20/g, ' ');
     return (
         <main>
-            <Suspense fallback={<div className='container m-auto my-10 text-azure-300 text-4xl text-center'>Loading...</div>}>
+            <Suspense fallback={
+                <div className="container m-auto mt-12 grid gap-8 opacity-60">
+                    <Skeleton className='w-full h-40 roudned' />
+                    <div className=''>
+                        {Array.from({ length: 20 }).map((_, index) => (
+                            <Skeleton key={index} className={`mb-3 w-1/2 h-5 rounded-full`} />
+                        ))}
+                    </div>
+                </div>
+            }>
                 <Entry inferredTitle={inferredTitle} user={user} entry={await getEntryBySlug(slug)} />
             </Suspense>
         </main>
