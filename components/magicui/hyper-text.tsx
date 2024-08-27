5352c14c5243f12eca2f9c02c34d47029db88ed8
Ted Waller <ted.waller.iii@gmail.com>
2024-08-26 22:39:23 -0400

    lint

--- components/magicui/hyper-text.tsx
+++ components/magicui/hyper-text.tsx
@@ -56,8 +56,8 @@ export default function HyperText({
                 ? l
                 : i <= interations.current
                   ? text[i]
-                  : alphabets[getRandomInt(26)]
-            )
+                  : alphabets[getRandomInt(26)],
+            ),
           );
           interations.current = interations.current + 0.1;
         } else {
@@ -65,7 +65,7 @@ export default function HyperText({
           clearInterval(interval);
         }
       },
-      duration / (text.length * 10)
+      duration / (text.length * 10),
     );
     // Clean up interval on unmount
     return () => clearInterval(interval);
@@ -98,7 +98,7 @@ export default function HyperText({
                 to-stone-900
                 rounded`
                 : "",
-              className
+              className,
             )}
             {...framerProps}
           >
